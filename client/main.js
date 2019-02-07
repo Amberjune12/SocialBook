621810fbc9582c734ca43b347a90fd240c281b55
JustBraf <dwaynecorey+github@gmail.com>
2019-02-07 15:41:58 -0400

    Added a modal for adding more profiles

--- client/main.js
+++ client/main.js
@@ -2,12 +2,32 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../lib/collections.js';
 
 Template.profile.events({
   'click .js-like'(event, instance) {
-    console.log("You clicked like");    
+	console.log("You clicked like");    
   },
   'click .js-dislike'(event, instance){
-  	alert("Clicked dislike");
+	alert("Clicked dislike");
+  }  
+});
+
+
+Template.addProfile.events({
+	'click .js-saveProfile'(event, instance){
+		// get user data
+		var fName = $("#exampleModal input[name='firstName']").val();
+		var lName = $("#exampleModal input[name='lastName']").val();
+		var image = $("#exampleModal input[name='image']").val();
+		console.log("The first name is",fName);
+		console.log("The last name is",lName);
+		console.log("The image is",image);
+		// Reset the form
+		$("#exampleModal input[name='firstName']").val('');
+		$("#exampleModal input[name='lastName']").val('');
+		$("#exampleModal input[name='image']").val('');
+		// Close the modal
+		$("#exampleModal").modal("hide");
   }
 });
