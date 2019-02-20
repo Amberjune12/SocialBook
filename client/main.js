76092ff87e8bac7053192aaa869d5b9ad03b0281
JustBraf <dwaynecorey+github@gmail.com>
2019-02-20 15:43:38 -0400

    Need to remember to commit better

--- client/main.js
+++ client/main.js
@@ -5,8 +5,8 @@ import './main.html';
 import '../lib/collections.js';
 
 Template.profiles.helpers({
-	profFname(){
-		return userDB.findOne({}).firstName;
+	profAll(){
+		return userDB.find({});
 	}
 });
 
@@ -16,7 +16,15 @@ Template.profiles.events({
   },
   'click .js-dislike'(event, instance){
 	alert("Clicked dislike");
-  }  
+  },
+  'click .js-delete'(event, instance){
+  	// console.log(this._id);
+  	var profID = this._id;
+  	$("#" + profID).fadeOut("slow", "swing", function () {
+  		userDB.remove({_id: profID});
+  	});
+  	
+  }
 });
 
 
@@ -26,9 +34,9 @@ Template.addProfile.events({
 		var fName = $("#exampleModal input[name='firstName']").val();
 		var lName = $("#exampleModal input[name='lastName']").val();
 		var image = $("#exampleModal input[name='image']").val();
-		console.log("The first name is",fName);
-		console.log("The last name is",lName);
-		console.log("The image is",image);
+		if (image == ""){
+			image="ninja.jpg";
+		}
 		// Reset the form
 		$("#exampleModal input[name='firstName']").val('');
 		$("#exampleModal input[name='lastName']").val('');
